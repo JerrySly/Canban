f9b1c309c5521c8ec473d51041ee322a452da744
JerrySly <eugeale@yandex.ru>
2022-07-05 01:11:09 +0300

    Стартовая версия(добавление колонок и карточек)

---
+++ main.js
@@ -0,0 +1,120 @@
+const columns = [];
+let editingColumn;
+class Column {
+  constructor(name) {
+    this.name = name;
+    this.cards = [];
+  }
+}
+class Card {
+  constructor(name, text, tag) {
+    this.name = name;
+    this.text = text;
+    this.tag = tag;
+  }
+}
+
+function fillData() {
+  let first = new Column("First");
+  first.cards.push(new Card("First card", "first text"));
+  first.cards.push(new Card("Second card", "Second text"));
+  columns.push(first);
+  let second = new Column("Second");
+  columns.push(second);
+}
+
+function addColumn() {
+  const dialog = document.getElementById("column-block");
+  let name = dialog.getElementsByTagName("input")[0].value;
+  columns.push(new Column(name));
+  paint();
+}
+
+function changeViewCardDialog(number, state) {
+  editingColumn = number;
+  const dialog = document.getElementById("card-block");
+  if (state) dialog.style.display = "block";
+  else dialog.style.display = "none";
+}
+
+function changeViewColumnDialog(state) {
+  const dialog = document.getElementById("column-block");
+  if (state) dialog.style.display = "block";
+  else dialog.style.display = "none";
+}
+
+function addCard() {
+  let inputs = document
+    .getElementById("card-block")
+    .getElementsByTagName("input");
+  let nameValue = inputs[0].value;
+  inputs[0].value = "";
+  let textValue = inputs[1].value;
+  inputs[1].value = "";
+  columns[editingColumn].cards.push(new Card(nameValue, textValue));
+  paint();
+}
+
+fillData();
+
+function paintColumn(name, index) {
+  let main = document.getElementById("main");
+  let column = document.createElement("div");
+  column.classList.add("column");
+  let head = document.createElement("div");
+  head.classList.add("head");
+
+  let nameBlock = document.createElement("div");
+  nameBlock.classList.add("name");
+  nameBlock.innerHTML = name;
+
+  let addButton = document.createElement("div");
+  addButton.innerHTML = "+";
+  addButton.classList.add("adding");
+
+  head.appendChild(nameBlock);
+  head.appendChild(addButton);
+  column.appendChild(head);
+
+  let list = document.createElement("div");
+  list.classList.add("list");
+  column.appendChild(list);
+
+  main.appendChild(column);
+  addButton.onclick = function () {
+    changeViewCardDialog(index, true);
+  };
+  return column;
+}
+
+function paintCard(columnIndex, cardInfo) {
+  let card = document.createElement("div");
+  let name = document.createElement("div");
+  name.classList.add("name");
+  name.innerHTML = cardInfo.name;
+  let text = document.createElement("div");
+  text.classList.add("text");
+  text.innerHTML = cardInfo.text;
+  card.appendChild(name);
+  card.appendChild(text);
+  card.classList.add("card");
+  document
+    .getElementsByClassName("column")
+    [columnIndex].getElementsByClassName("list")[0]
+    .appendChild(card);
+}
+
+function deleteContent() {
+  document.getElementById("main").innerHTML = "";
+}
+
+function paint() {
+  deleteContent();
+  columns.forEach((x, index) => {
+    paintColumn(x.name, index);
+    x.cards.forEach((y) => {
+      paintCard(index, y);
+    });
+  });
+}
+paint();
